228c4b5173c1700b060c6b87c3f7df82d61274de
Vedran Tepavcevic <mr_vedran@hotmail.com>
2019-05-31 14:26:26 -0400

    Add contract file and modify test file

---
+++ Inheritance-Project/contracts/TipJarwithInheritance.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.5.4;
+
+// this interface defines an event and a function for later use
+interface eventInterface {
+    event logOwnerChange(address oldOwner, address newOwner);
+    function changeOwner(address payable _newOwner) external;
+}
+
+// this contract defines an abstract function for later use
+contract ownerAbstract {
+    address payable owner;
+    function showOwner() public view returns (address);
+}
+
+// this contract inherits from the Interface and the contract that defines an abstract function
+// this defines owner related functions
+contract Owner is eventInterface, ownerAbstract {
+    address payable owner;
+    constructor () public {
+        owner = msg.sender;
+    }
+    function changeOwner(address payable _newOwner) public {
+        require(msg.sender == owner, "caller is not owner");
+        owner = _newOwner;
+        emit logOwnerChange(msg.sender, _newOwner);
+    }
+    function showOwner() public view returns (address) {
+        return owner;
+    }
+}
+
+// this contract inherits from the contract that defines owner functions
+// this defines tipJar functions
+contract TipJar is Owner {
+    function() external payable {
+    }
+    function withdraw() public {
+        require(msg.sender == owner, "caller is not owner");
+        owner.transfer(address(this).balance);
+    }
+}
--- Inheritance-Project/test/contract_spec.js
+++ Inheritance-Project/test/contract_spec.js
@@ -1,6 +1,6 @@
 // /*global contract, config, it, assert*/
-/*
-const SimpleStorage = require('Embark/contracts/SimpleStorage');
+
+const TipJar = require('Embark/contracts/TipJar');
 
 let accounts;
 
@@ -13,31 +13,17 @@ config({
   //  ]
   //},
   contracts: {
-    "SimpleStorage": {
-      args: [100]
-    }
+    TipJar: {}
   }
 }, (_err, web3_accounts) => {
   accounts = web3_accounts
 });
 
-contract("SimpleStorage", function () {
+contract("TipJar", function () {
   this.timeout(0);
-
-  it("should set constructor value", async function () {
-    let result = await SimpleStorage.methods.storedData().call();
-    assert.strictEqual(parseInt(result, 10), 100);
+  
+  it("get owner address", async function () {
+    let result = await TipJar.methods.showOwner().call();
+    assert.ok(result);
   });
-
-  it("set storage value", async function () {
-    await SimpleStorage.methods.set(150).send();
-    let result = await SimpleStorage.methods.get().call();
-    assert.strictEqual(parseInt(result, 10), 150);
-  });
-
-  it("should have account with balance", async function() {
-    let balance = await web3.eth.getBalance(accounts[0]);
-    assert.ok(parseInt(balance, 10) > 0);
-  });
-}
-*/
+})
