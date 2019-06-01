bc93ffbeef4c5ffd07927c1860ad7435e51f369a
Vedran Tepavcevic <mr_vedran@hotmail.com>
2019-06-01 09:07:50 -0400

    Modify Test file

--- Inheritance-Project/test/contract_spec.js
+++ Inheritance-Project/test/contract_spec.js
@@ -22,8 +22,24 @@ config({
 contract("TipJar", function () {
   this.timeout(0);
   
+  before(async function() {
+    accounts = await web3.eth.getAccounts();
+  });
+
   it("get owner address", async function () {
     let result = await TipJar.methods.showOwner().call();
+    assert.equal(result, accounts[0]);
+  });
+
+  it("change owner address", async function () {
+    let result = await TipJar.methods.changeOwner(accounts[1]).send();
+    let log = result.events.logOwnerChange;
+    assert.notEqual(log.returnValues[0], log.returnValues[1]);
+  });
+
+  it("Withdraw form TipJar", async function () {
+    let result = await TipJar.methods.withdraw().send({from: accounts[1]});
     assert.ok(result);
   });
+
 })
