a9bbcd1364539bc957748a8de81afc4f8b390622
Vedran Tepavcevic <mr_vedran@hotmail.com>
2019-06-05 21:19:45 -0400

    Add negative test cases

--- Inheritance-Project/test/contract_spec.js
+++ Inheritance-Project/test/contract_spec.js
@@ -2,6 +2,14 @@
 
 const TipJar = require('Embark/contracts/TipJar');
 
+function didRevertCorrectly(actualError, expectedError) {
+  return actualError.includes(expectedError);
+}
+
+let expectedErrorMessages = {
+  "owner": "caller is not owner"
+}
+
 let accounts;
 
 // For documentation please see https://embark.status.im/docs/contracts_testing.html
@@ -21,25 +29,38 @@ config({
 
 contract("TipJar", function () {
   this.timeout(0);
-  
-  before(async function() {
-    accounts = await web3.eth.getAccounts();
-  });
 
   it("get owner address", async function () {
     let result = await TipJar.methods.showOwner().call();
     assert.equal(result, accounts[0]);
   });
 
-  it("change owner address", async function () {
+  it("owner can change owner address", async function () {
     let result = await TipJar.methods.changeOwner(accounts[1]).send();
     let log = result.events.logOwnerChange;
     assert.notEqual(log.returnValues[0], log.returnValues[1]);
   });
 
-  it("Withdraw form TipJar", async function () {
+
+  it("non-owner cannot change owner address", async function () {
+    try {
+      await TipJar.methods.changeOwner(accounts[1]).send();
+    } catch (error) {
+      assert.ok(didRevertCorrectly(error.message,expectedErrorMessages["owner"]))
+    }
+  });
+
+  it("owner can withdraw from TipJar", async function () {
     let result = await TipJar.methods.withdraw().send({from: accounts[1]});
     assert.ok(result);
+  });
+
+  it("non-owner cannot withdraw from TipJar", async function () {
+    try {
+    await TipJar.methods.withdraw().send();
+  } catch (error) {
+    assert.ok(didRevertCorrectly(error.message,expectedErrorMessages["owner"]))
+  }
   });
 
 })
